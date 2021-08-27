85d6ed697e5e1d433e1744b0c56991597629ef1e
SuperSonic <supersonic@livemail.tw>
2021-08-27 14:08:37 +0800

    feat: update COPYING

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'PopCat Active',
+    title: 'PopSenzawa',
     htmlAttrs: {
       lang: 'en',
     },
